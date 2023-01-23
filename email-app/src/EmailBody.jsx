3a56664ba0420fd72250738e8d7ac960ad09c7a8
Samyak Khairnar <samyakkhairnar@rocketmail.com>
2023-01-23 23:56:37 +0530

    Creating Email Body

---
+++ email-app/src/EmailBody.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+const EMAIL_BODY_URL = 'https://flipkart-email-mock.vercel.app/?id=';
+
+function EmailBody() {
+  return (
+    <div>
+        Email Body
+    </div>
+  )
+}
+
+export default EmailBody
